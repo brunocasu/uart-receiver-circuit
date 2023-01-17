6e8685f533f402e26ffd652e7bcfcbd91d046667
bruno casu <brunocasu8@gmail.com>
2023-01-17 23:36:31 +0100

    re work on synch block - using fsm to track state changes - synch block done

--- model_sim_prj/rx_synch.vhd
+++ model_sim_prj/rx_synch.vhd
@@ -5,177 +5,201 @@ entity rx_synch is
     generic(
         OS_RATE     : natural := 8; -- clock over sampling rate (UART bit rate B = 115200)
         WORD_SIZE   : natural := 7; -- UART word size (W = 7) - does not count the parity bit
-        STOP_BITS   : natural := 2; -- number of expected stop bits (S = 2)
-        BREAK_COUNT : natural := 11 -- number of consecutive '0's to detect a BREAK
+        STOP_BITS   : natural := 2 -- number of expected stop bits (S = 2)
+        -- BREAK_COUNT : natural := 11 -- number of consecutive '0's to detect a BREAK
     );
     port (
         clk   : in std_logic;
         rst   : in std_logic;
         en    : in std_logic;
         rx    : in std_logic;
-        clear : in std_logic;
-        sampled_rx    : out std_logic_vector(WORD_SIZE downto 0); -- parallel output includes parity bit
-        break_status  : out std_logic;
-        synch_status  : out std_logic_vector(3 downto 0)
-        -- break_status    : break error flag
-        -- synch status(2) : frame error flag
-        -- synch status(1) : end of frame
-        -- synch status(0) : word received flag
+        data_out    : out std_logic_vector(WORD_SIZE downto 0); -- parallel output includes parity bit
+        data_ready_out  : out std_logic;
+        frame_start_out : out std_logic;
+        frame_stop_out  : out std_logic;
+        frame_error_out : out std_logic
 
     );
 end entity;
 
 architecture struct of rx_synch is
 
-    signal clk_count    : integer range 0 to OS_RATE := 0;
+    type rx_synch_fsm_state_t is
+    (
+        RESET_S,
+        IDLE_S,
+        START_DETECT_S,
+        RECEIVE_DATA_S,
+        STOP_DETECT_S,
+        FRAME_ERROR_S
+    );
+
+    signal rx_synch_fsm_state : rx_synch_fsm_state_t := RESET_S;
     signal bit_count    : integer range 0 to WORD_SIZE + STOP_BITS + 2 + 1 := 0;
-    signal start_count  : integer range 0 to OS_RATE/2 := 0;
-    signal idle_flag    : std_logic := '1'; -- 1 for idle
-    signal break_out    : std_logic := '0';
-    signal dff_out      : std_logic := '0';
-    signal end_of_frame : std_logic := '0';
 
-    component break_counter is
-        generic(
-            OS_RATE     : natural := 8; -- clock over sampling rate (UART bit rate B = 115200)
-            BREAK_COUNT : natural := 11 -- number of consecutive '0's to detect a BREAK
-        );
-        port (
-            clk     : in std_logic;
-            rst     : in std_logic;
-            rx      : in std_logic;
-            status  : out std_logic
-        );
-    end component;
+    begin
 
-    --type rx_synch_fsm_state_t is
-    --(
-    --    RESET,
-    --    IDLE,
-    --    DETECT_START_BIT,
-    --    RECEIVE_DATA,
-    --    RECEIVE_PARITY,
-    --    PUBLISH_RESULT,
-    --    ERROR_FRAME
-    --);
+        p_rx_synch_fsm : process(clk, rst)
+        variable count : integer := 0;
+        begin
+            if rst = '0' then -- reset active low
+                rx_synch_fsm_state <= RESET_S;
+                data_out <= (others => '0');
+                data_ready_out   <= '0';
+                frame_start_out  <= '0';
+                frame_stop_out   <= '0';
+                frame_error_out  <= '0';
+                bit_count    <= 0;
+
+            elsif rising_edge(clk) then
+                case rx_synch_fsm_state is
+                    when RESET_S =>
+                        if rx = '1' then
+                            rx_synch_fsm_state <= IDLE_S;
+                        end if;
+                    when IDLE_S =>
+                        data_ready_out <= '0';
+                        frame_stop_out <= '1';
+                        if rx = '0' then
+                            rx_synch_fsm_state <= START_DETECT_S;
+                            count := 1;
+                        end if;
+                    when START_DETECT_S =>
+                        frame_stop_out <= '0';
+                        if count = (OS_RATE/2) - 1 then
+                            if rx = '0' then
+                                rx_synch_fsm_state <= RECEIVE_DATA_S;
+                                count := 0;
+                            else
+                                rx_synch_fsm_state <= FRAME_ERROR_S;
+                            end if;
+                        else
+                            count := count + 1;
+                        end if;
+                    when RECEIVE_DATA_S =>
+                        frame_start_out <= '1';
+                        if count = OS_RATE - 1 then
+                            count := 0;
+                            if bit_count < WORD_SIZE then
+                                data_out(bit_count) <= rx;
+                            end if;
+                            bit_count <= bit_count + 1;
+                            -- if bit_count-1 >= 0 and bit_count-1 <= WORD_SIZE + 1 then
+                            -- end if;
+                            if bit_count = WORD_SIZE then
+                                rx_synch_fsm_state <= STOP_DETECT_S;
+                            end if;
+                        else
+                            count := count + 1;
+                        end if;
+                    when STOP_DETECT_S =>
+                        frame_start_out <= '0';
+                        data_ready_out <= '1';
+                        count := count + 1;
+                        if count = OS_RATE then
+                            count := 0;
+                            if rx = '1' then
+                                if bit_count = WORD_SIZE + STOP_BITS then
+                                    rx_synch_fsm_state <= IDLE_S;
+                                    bit_count  <= 0;
+                                    data_out <= (others => '0');
+                                else
+                                    bit_count <= bit_count + 1;
+                                end if;
+                            else
+                                rx_synch_fsm_state <= FRAME_ERROR_S;
+                            end if;
+                        end if;
+                    when FRAME_ERROR_S =>
+                        data_ready_out <= '0';
+                        frame_error_out <= '1';
+
+                    when others => null;
+                end case;
+            end if;
+        end process;
+end architecture;
+    --p_rx_synch : process(clk, rst)
+    --variable test : integer := 0;
     --
-    --signal rx_synch_fsm_state : rx_synch_fsm_state_t := IDLE;
-
-
-
-begin
-
-
-    --p_rx_synch_fsm : process(clk, rst)
     --begin
     --    if rst = '0' then -- reset active low
-    --
+    --        data_out <= (others => '0');
+    --        synch_status <= (others => '0');
+    --        clk_count    <= 0;
+    --        bit_count    <= 0;
+    --        start_count  <= 0;
     --    elsif rising_edge(clk) then
-    --        case rx_synch_fsm_state is
-    --            when IDLE =>
-    --                if (blah = '1') then
-    --                    rx_synch_fsm_state <= DETECT_START_BIT;
+    --        if en = '1' then
+    --            -- rx sampling
+    --            if bit_count > 0  then
+    --                clk_count <=  clk_count + 1;
+    --                if clk_count = OS_RATE - 1 then
+    --                    clk_count <= 0;
+    --                    if bit_count <= WORD_SIZE + 1 then
+    --                        data_out(bit_count-1) <= rx; -- sample input to vector
+    --                        bit_count <= bit_count + 1;
+    --                    else -- check stop bits
+    --                        synch_status(0) <= '0';
+    --                        if rx = '1' then
+    --                            bit_count <= bit_count + 1;
+    --                        else
+    --                            bit_count <= 0;
+    --                            synch_status(2) <= '1'; -- frame error
+    --                        end if;
+    --                    end if;
     --                end if;
-    --        end case;
+    --            else
+    --                if end_of_frame = '1' then -- clear end of frame
+    --                    synch_status(1) <= '0';
+    --                end if;
+    --            end if;
+    --
+    --            -- end of word (include parity bit)
+    --            if bit_count = WORD_SIZE + 2 then
+    --                synch_status(0) <= '1';
+    --            end if;
+    --
+    --            -- end of frame detection
+    --            if bit_count = WORD_SIZE + STOP_BITS + 2 and rx = '1' then
+    --                bit_count <= 0;
+    --                synch_status(1) <= '1';
+    --                IDLE_S_flag <= '1';
+    --                data_out <= (others => '0');
+    --                end_of_frame <= '1';
+    --            end if;
+    --
+    --            -- -- after a frame error detection the Control block clear the synchronizer ot receive again
+    --            if clear = '1' and IDLE_S_flag = '0' then
+    --                IDLE_S_flag <= '1';
+    --                synch_status(0) <= '0';
+    --                synch_status(2) <= '0';
+    --            end if;
+    --
+    --            -- UART start synchronization
+    --            if (rx = '0' and start_count = 0 and IDLE_S_flag = '1') then
+    --                start_count <= start_count + 1;
+    --                synch_status(1) <= '0';
+    --                -- synch_status(2) <= '0';
+    --                -- frame_error_out_flag <= '0';
+    --            end if;
+    --
+    --            -- UART start bit detection
+    --            if (start_count > 0 and IDLE_S_flag = '1') then
+    --                start_count <= start_count + 1;
+    --                -- first zero check
+    --                if start_count = (OS_RATE/2)-1 then
+    --                    start_count <= 0;
+    --                    if rx = '0' then
+    --                        bit_count <= 1;
+    --                        IDLE_S_flag <= '0'; -- exited IDLE_S state
+    --                    else
+    --                        synch_status(2) <= '1'; -- frame error
+    --                    end if;
+    --                end if;
+    --            end if;
+    --        end if;
     --    end if;
     --end process;
 
-    break_DUT: break_counter
-        generic map(
-            OS_RATE => OS_RATE,
-            BREAK_COUNT => BREAK_COUNT
-        )
-        port map(
-            clk => clk,
-            rst => rst,
-            rx  => rx,
-            status => break_out
-        );
 
-    break_status <= break_out;
-    synch_status(3) <= break_out;
-
-    p_rx_synch : process(clk, rst)
-    variable test : integer := 0;
-
-    begin
-        if rst = '0' then -- reset active low
-            sampled_rx <= (others => '0');
-            synch_status <= (others => '0');
-            clk_count    <= 0;
-            bit_count    <= 0;
-            start_count  <= 0;
-        elsif rising_edge(clk) then
-            if en = '1' then
-                -- rx sampling
-                if bit_count > 0  then
-                    clk_count <=  clk_count + 1;
-                    if clk_count = OS_RATE - 1 then
-                        clk_count <= 0;
-                        if bit_count <= WORD_SIZE + 1 then
-                            sampled_rx(bit_count-1) <= rx; -- sample input to vector
-                            bit_count <= bit_count + 1;
-                        else -- check stop bits
-                            synch_status(0) <= '0';
-                            if rx = '1' then
-                                bit_count <= bit_count + 1;
-                            else
-                                bit_count <= 0;
-                                synch_status(2) <= '1'; -- frame error
-                            end if;
-                        end if;
-                    end if;
-                else
-                    if end_of_frame = '1' then -- clear end of frame
-                        synch_status(1) <= '0';
-                    end if;
-                end if;
-
-                -- end of word (include parity bit)
-                if bit_count = WORD_SIZE + 2 then
-                    synch_status(0) <= '1';
-                end if;
-
-                -- end of frame detection
-                if bit_count = WORD_SIZE + STOP_BITS + 2 and rx = '1' then
-                    bit_count <= 0;
-                    synch_status(1) <= '1';
-                    idle_flag <= '1';
-                    sampled_rx <= (others => '0');
-                    end_of_frame <= '1';
-                end if;
-
-                -- -- after a frame error detection the Control block clear the synchronizer ot receive again
-                if clear = '1' and idle_flag = '0' then
-                    idle_flag <= '1';
-                    synch_status(0) <= '0';
-                    synch_status(2) <= '0';
-                end if;
-
-                -- UART start synchronization
-                if (rx = '0' and start_count = 0 and idle_flag = '1') then
-                    start_count <= start_count + 1;
-                    synch_status(1) <= '0';
-                    -- synch_status(2) <= '0';
-                    -- frame_error_flag <= '0';
-                end if;
-
-                -- UART start bit detection
-                if (start_count > 0 and idle_flag = '1') then
-                    start_count <= start_count + 1;
-                    -- first zero check
-                    if start_count = (OS_RATE/2)-1 then
-                        start_count <= 0;
-                        if rx = '0' then
-                            bit_count <= 1;
-                            idle_flag <= '0'; -- exited idle state
-                        else
-                            synch_status(2) <= '1'; -- frame error
-                        end if;
-                    end if;
-                end if;
-            end if;
-        end if;
-    end process;
-
-end architecture;
--- model_sim_prj/tb_rx_synch.vhd
+++ model_sim_prj/tb_rx_synch.vhd
@@ -11,38 +11,30 @@ architecture bhv of tb_rx_synch is
   constant RESET_TIME : time    := 200 ns;
   constant OS_RATE    : natural := 8;
   constant FRAME_SIZE : natural := 11; -- fixed for W=7 and S=2
-  constant BREAK_COUNT : natural := 11; -- number of consecutive '0's to detect a BREAK
 
   signal clk_tb       : std_logic := '0';
   signal rst_tb       : std_logic := '0';
   signal rx_tb        : std_logic := '1';
   signal en_tb        : std_logic := '1';
-  signal clear_tb     : std_logic := '0';
-  signal break_status_out_tb     : std_logic := '0';
-  signal status_out_tb  : std_logic_vector(3 downto 0) := (others => '0');
-  signal sampled_out_tb : std_logic_vector(7 downto 0) := (others => '0'); -- fixed for W=7
+  signal data_out_tb : std_logic_vector(7 downto 0) := (others => '0'); -- fixed for W=7
   signal end_sim      : std_logic := '1';
-
-  -- test words ()
-  signal w1_test : std_logic_vector(FRAME_SIZE-1 downto 0) := "11010101010";
-  signal w2_test : std_logic_vector(FRAME_SIZE-1 downto 0) := "11001111110";
 
   component rx_synch is
     generic(
         OS_RATE     : natural := 8; -- clock over sampling rate (UART bit rate B = 115200)
         WORD_SIZE   : natural := 7; -- UART word size (W = 7) - does not count the parity bit
-        STOP_BITS   : natural := 2; -- number of expected stop bits (S = 2)
-        BREAK_COUNT : natural := 11 -- number of consecutive '0's to detect a BREAK
+        STOP_BITS   : natural := 2 -- number of expected stop bits (S = 2)
     );
     port (
         clk   : in std_logic;
         rst   : in std_logic;
         en    : in std_logic;
-        clear : in std_logic;
         rx    : in std_logic;
-        sampled_rx    : out std_logic_vector(WORD_SIZE downto 0); -- parallel output includes parity bit
-        break_status  : out std_logic;
-        synch_status  : out std_logic_vector(3 downto 0)
+        data_out    : out std_logic_vector(WORD_SIZE downto 0); -- parallel output includes parity bit
+        data_ready_out  : out std_logic;
+        frame_start_out : out std_logic;
+        frame_stop_out  : out std_logic;
+        frame_error_out : out std_logic
     );
   end component;
 
@@ -56,18 +48,16 @@ begin
       clk       => clk_tb,
       rst       => rst_tb,
       en        => en_tb,
-      clear     => clear_tb,
       rx        => rx_tb,
-      break_status => break_status_out_tb,
-      synch_status    => status_out_tb,
-      sampled_rx      => sampled_out_tb
+      data_out      => data_out_tb
+      --data_ready_out
+      --frame_start_out
+      --frame_stop_out
+      --frame_error_out
     );
 
   stimuli: process(clk_tb, rst_tb)
-    variable simulation_counter : integer := 0;
     variable test_counter : integer := 0;
-    variable word_counter : integer := 0;
-    variable word_id      : integer := 0;
 
   begin
     if (rst_tb = '0') then
@@ -89,20 +79,18 @@ begin
         when 81 => rx_tb  <= '1'; -- stop 2
         -- asynch TX
         when 189  => rx_tb  <= '0'; -- start bit
-        when 197  => rx_tb  <= '0';-- w0
+        when 197  => rx_tb  <= '1';-- w0
         when 205 => rx_tb   <= '0';-- w1
-        when 213 => rx_tb   <= '0';-- w2
+        when 213 => rx_tb   <= '1';-- w2
         when 221 => rx_tb   <= '0';-- w3
-        when 229 => rx_tb   <= '0';-- w4
+        when 229 => rx_tb   <= '1';-- w4
         when 237 => rx_tb   <= '0';-- w5
-        when 245 => rx_tb   <= '0';-- w6
+        when 245 => rx_tb   <= '1';-- w6
         when 253 => rx_tb   <= '0';-- parity bit
-        when 261 => rx_tb   <= '0';-- stop 1 !!!FRAME ERROR!!!
+        when 261 => rx_tb   <= '1';-- stop 1 !!!FRAME ERROR!!!
         when 269 => rx_tb   <= '0';-- stop 2
 
-        when 277 => rx_tb    <= '1';
-        when 279 => clear_tb    <= '1';
-        when 280 => clear_tb    <= '0';
+        -- when 277 => rx_tb    <= '1';
 
         when 301 => rx_tb  <= '0'; -- start bit
         when 309 => rx_tb  <= '1'; -- w0
