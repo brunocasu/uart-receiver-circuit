5a753889a5effb14b55729b8477ddc6e555f8670
bruno casu <brunocasu8@gmail.com>
2023-01-17 17:25:45 +0100

    fixed break module error - synch + break OK

--- model_sim_prj/tb_multi_frame_rx_synch.vhd
+++ model_sim_prj/tb_multi_frame_rx_synch.vhd
@@ -18,8 +18,7 @@ architecture bhv of tb_rx_synch is
   signal rx_tb        : std_logic := '1';
   signal en_tb        : std_logic := '1';
   signal clear_tb     : std_logic := '0';
-  signal break_status_out_tb     : std_logic := '0';
-  signal status_out_tb  : std_logic_vector(3 downto 0) := (others => '0');
+  signal status_out_tb  : std_logic_vector(2 downto 0) := (others => '0');
   signal sampled_out_tb : std_logic_vector(7 downto 0) := (others => '0'); -- fixed for W=7
   signal end_sim      : std_logic := '1';
 
@@ -31,7 +30,7 @@ architecture bhv of tb_rx_synch is
     generic(
         OS_RATE     : natural := 8; -- clock over sampling rate (UART bit rate B = 115200)
         WORD_SIZE   : natural := 7; -- UART word size (W = 7) - does not count the parity bit
-        STOP_BITS   : natural := 2; -- number of expected stop bits (S = 2)
+        STOP_BITS   : natural := 2 -- number of expected stop bits (S = 2)
         BREAK_COUNT : natural := 11 -- number of consecutive '0's to detect a BREAK
     );
     port (
@@ -41,7 +40,6 @@ architecture bhv of tb_rx_synch is
         clear : in std_logic;
         rx    : in std_logic;
         sampled_rx    : out std_logic_vector(WORD_SIZE downto 0); -- parallel output includes parity bit
-        break_status  : out std_logic;
         synch_status  : out std_logic_vector(3 downto 0)
     );
   end component;
@@ -58,7 +56,6 @@ begin
       en        => en_tb,
       clear     => clear_tb,
       rx        => rx_tb,
-      break_status => break_status_out_tb,
       synch_status    => status_out_tb,
       sampled_rx      => sampled_out_tb
     );
@@ -75,52 +72,59 @@ begin
       test_counter := 0;
     elsif (rising_edge(clk_tb)) then
 
-      case(test_counter) is
-        when 1  => rx_tb  <= '0'; -- start bit
-        when 9  => rx_tb  <= '1'; -- w0
-        when 17 => rx_tb  <= '0'; -- w1
-        when 25 => rx_tb  <= '1'; -- w2
-        when 33 => rx_tb  <= '0'; -- w3
-        when 41 => rx_tb  <= '1'; -- w4
-        when 49 => rx_tb  <= '0'; -- w5
-        when 57 => rx_tb  <= '1'; -- w6
-        when 65 => rx_tb  <= '0'; -- parity bit
-        when 73 => rx_tb  <= '1'; -- stop 1
-        when 81 => rx_tb  <= '1'; -- stop 2
-        -- asynch TX
-        when 189  => rx_tb  <= '0'; -- start bit
-        when 197  => rx_tb  <= '0';-- w0
-        when 205 => rx_tb   <= '0';-- w1
-        when 213 => rx_tb   <= '0';-- w2
-        when 221 => rx_tb   <= '0';-- w3
-        when 229 => rx_tb   <= '0';-- w4
-        when 237 => rx_tb   <= '0';-- w5
-        when 245 => rx_tb   <= '0';-- w6
-        when 253 => rx_tb   <= '0';-- parity bit
-        when 261 => rx_tb   <= '0';-- stop 1 !!!FRAME ERROR!!!
-        when 269 => rx_tb   <= '0';-- stop 2
+        if test_counter = OS_RATE - 1 then
+            test_counter := 0;
+            case (word_id) is
+                when 0 => rx_tb <= w1_test(word_counter);
+                when 1 => rx_tb <= w2_test(word_counter);
+                when others => null;
+            end case;
+            word_counter := word_counter + 1;
+            if word_counter = FRAME_SIZE then
+                word_counter := 0;
+                case (word_id) is
+                    when 0 => word_id := 1;
+                    when 1 => word_id := 0;
+                    when others => null;
+                end case;
+            end if;
+        else
+            test_counter := test_counter + 1;
+        end if;
+      --case(test_counter) is
+      --  when 1  => rx_tb  <= '0'; -- start bit
+      --  when 9  => rx_tb  <= '1'; -- w0
+      --  when 17 => rx_tb  <= '0'; -- w1
+      --  when 25 => rx_tb  <= '1'; -- w2
+      --  when 33 => rx_tb  <= '0'; -- w3
+      --  when 41 => rx_tb  <= '1'; -- w4
+      --  when 49 => rx_tb  <= '0'; -- w5
+      --  when 57 => rx_tb  <= '1'; -- w6
+      --  when 65 => rx_tb  <= '0'; -- parity bit
+      --  when 73 => rx_tb  <= '1'; -- stop 1
+      --  when 81 => rx_tb  <= '1'; -- stop 2
+      --
+      --  when 189  => rx_tb   <= '0'; -- start bit
+      --  when 197  => rx_tb   <= '1';-- w0
+      --  when 205 => rx_tb   <= '1';-- w1
+      --  when 213 => rx_tb   <= '1';-- w2
+      --  when 221 => rx_tb   <= '1';-- w3
+      --  when 229 => rx_tb   <= '1';-- w4
+      --  when 237 => rx_tb   <= '1';-- w5
+      --  when 245 => rx_tb   <= '0';-- w6
+      --  when 253 => rx_tb   <= '0';-- parity bit
+      --  when 261 => rx_tb   <= '1';-- stop 1
+      --  when 269 => rx_tb   <= '1';-- stop 2
+      --
+      --  when 277 => rx_tb    <= '1';
+      --  when 280 => end_sim  <= '0';
+      --  when others => null;
+      --end case;
 
-        when 277 => rx_tb    <= '1';
-        when 279 => clear_tb    <= '1';
-        when 280 => clear_tb    <= '0';
-
-        when 301 => rx_tb  <= '0'; -- start bit
-        when 309 => rx_tb  <= '1'; -- w0
-        when 317 => rx_tb  <= '0'; -- w1
-        when 325 => rx_tb  <= '1'; -- w2
-        when 333 => rx_tb  <= '0'; -- w3
-        when 341 => rx_tb  <= '1'; -- w4
-        when 349 => rx_tb  <= '0'; -- w5
-        when 357 => rx_tb  <= '1'; -- w6
-        when 365 => rx_tb  <= '0'; -- parity bit
-        when 373 => rx_tb  <= '1'; -- stop 1
-        when 381 => rx_tb  <= '1'; -- stop 2
-
-        when 400 => end_sim  <= '0';
-        when others => null;
-      end case;
-
-    test_counter := test_counter + 1;
+    simulation_counter := simulation_counter +1;
+        if simulation_counter = 800 then
+            end_sim  <= '0';
+        end if;
     end if;
   end process;
 
